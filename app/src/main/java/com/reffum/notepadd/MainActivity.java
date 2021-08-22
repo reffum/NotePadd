163c407df041c63afe0b7cf7a39c2d7f11dac871
Oleg Gavrilchenko <reffum@bk.ru>
2021-08-22 19:28:10 +0300

    Finish all main functions
    - Write text
    - Save file
    - Open file

--- app/src/main/java/com/reffum/notepadd/MainActivity.java
+++ app/src/main/java/com/reffum/notepadd/MainActivity.java
@@ -1,27 +1,126 @@
 package com.reffum.notepadd;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.reffum.notepadd.databinding.ActivityMainBinding;
 
+import android.os.ParcelFileDescriptor;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+
+    /**
+     * Current edited file
+     */
+    FileDescriptor fileDescriptor = null;
+
+    /**
+     * Activity launcher for open existing document
+     */
+    private final ActivityResultLauncher<String[]> openDocumentLauncher = registerForActivityResult(
+            new ActivityResultContracts.OpenDocument(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+                    String fileName = result.getPath();
+                    try {
+                        fileDescriptor = getContentResolver().openFileDescriptor(result, "rw").getFileDescriptor();
+
+                        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileDescriptor));
+
+                        StringBuilder stringBuiler = new StringBuilder();
+
+                        String line;
+                        while(true) {
+                            line = bufferedReader.readLine();
+                            if(line == null)
+                                break;
+
+                            stringBuiler.append(line).append(System.lineSeparator());
+                        }
+
+                        binding.editText.setText(stringBuiler.toString());
+                        binding.toolbar.setTitle(fileName);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "File " + fileName + " not found",
+                                Toast.LENGTH_SHORT).show();
+                    }catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Read file " + result.getPath() + " error.",
+                                Toast.LENGTH_SHORT
+                                ).show();
+                    }
+                }
+            }
+    );
+
+    /**
+     * Activity launcher for create new document
+     */
+    private final ActivityResultLauncher<String> createDocumentLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.CreateDocument(),
+                    new ActivityResultCallback<Uri>() {
+                        @Override
+                        public void onActivityResult(Uri result) {
+                            try {
+                                fileDescriptor = getContentResolver()
+                                        .openFileDescriptor(result, "rw")
+                                        .getFileDescriptor();
+
+                                // Write editText content in the file
+                                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileDescriptor));
+                                bufferedWriter.write(binding.editText.getText().toString());
+                                bufferedWriter.flush();
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Can not open file " + result.getPath(),
+                                        Toast.LENGTH_SHORT
+                                        ).show();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Write to file " + result.getPath() + " error",
+                                        Toast.LENGTH_SHORT
+                                ).show();
+                            }
+                        }
+                    }
+            );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +130,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -60,17 +147,68 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_new) {
+
+            // Save text to file. Close file and clear current descriptor. Clear text.
+            saveTextToFile();
+            binding.editText.getText().clear();
+            fileDescriptor = null;
+
             return true;
+        }
+        else if(id == R.id.action_open) {
+            openDocumentLauncher.launch(new String[]{"*/*"});
+            return true;
+        }
+        else if(id == R.id.action_save) {
+            if(fileDescriptor == null)
+                createDocumentLauncher.launch("new_file.txt");
+            else {
+                saveTextToFile();
+            }
+
+            return true;
+        }
+        else if(id == R.id.action_save_as) {
+            saveTextToFile();
+            createDocumentLauncher.launch("new_file.txt");
+        }
+        else if(id == R.id.action_bug_report) {
+            sendBugReportEmail();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    private void saveTextToFile()
+    {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileDescriptor));
+
+            // Truncate file
+            FileOutputStream fileOutputStream = new FileOutputStream(fileDescriptor);
+            fileOutputStream.getChannel().truncate(0);
+
+            // Write EditText content to file
+            String notePadText = binding.editText.getText().toString();
+            bufferedWriter.write(notePadText);
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Write to file error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void sendBugReportEmail()
+    {
+        String uriText = "mailto:reffum@bk.ru" +
+                "?subject=" + Uri.encode("NotePadd bug report") +
+                "&body" + Uri.encode("Application BUG description");
+
+        Uri uri = Uri.parse(uriText);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(uri);
+        startActivity(Intent.createChooser(intent, "Send email"));
     }
 }
